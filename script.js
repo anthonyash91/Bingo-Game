ec9492735d4831afe0f364e0706295120c586e00
Anthony Ashcraft <anthonyashcraft91@gmail.com>
2022-09-11 03:16:56 -0400

    changed up the JS so that the code is shorter

--- script.js
+++ script.js
@@ -6,105 +6,95 @@ columnO = document.querySelectorAll('.column-o')
 
 const generateBoardNums = () => {
 	// column b numbers
-	const numbers = {
-		b: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-		i: [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
-		n: [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45],
-		g: [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
-		o: [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]
-	},
-	squares = document.querySelectorAll('.square'),
-	squareSpans = document.querySelectorAll('.square span');
+	const numbers = {b: [], i: [], n: [], g: [], o: []},
+	squares = document.querySelectorAll('.square');
+
+	squares.forEach(element => {
+		element.classList.replace('show-number', 'hide-number');
+	})
+
+	// create the array with the numbers that will be randomly picked and assigned to spots in the B column
+	for(let i = 1; i <= 15; i++) numbers.b.push(i);
+	// create the array with the numbers that will be randomly picked and assigned to spots in the I column
+	for(let i = 16; i <= 30; i++) numbers.i.push(i);
+	// create the array with the numbers that will be randomly picked and assigned to spots in the N column
+	for(let i = 31; i <= 45; i++) numbers.n.push(i);
+	// create the array with the numbers that will be randomly picked and assigned to spots in the G column
+	for(let i = 46; i <= 60; i++) numbers.g.push(i);
+	// create the array with the numbers that will be randomly picked and assigned to spots in the O column
+	for(let i = 61; i <= 75; i++) numbers.o.push(i);
 
 	for(let i = 0; i < columnB.length; i++) {
-		let randomizeNums = Math.floor(Math.random() * numbers.b.length),
-		randomNum = numbers.b[randomizeNums],
-		removeRandomNum = numbers.b.indexOf(randomNum);
-		numbers.b.splice(removeRandomNum, 1);
-		bingoSquare = columnB[i];
-		bingoNum = document.createElement('span');
-		bingoNum.classList.add('number', 'hide');
-		bingoNum.innerText = randomNum;
-		bingoSquare.appendChild(bingoNum);
-		bingoNum.parentNode.setAttribute('data', `B${randomNum}`);
+		let randomizeNums = Math.floor(Math.random() * numbers.b.length);
+		// pick a random number from the b array
+		let randomNum = numbers.b[randomizeNums];
+		let randomNumFade = Math.floor(Math.random() * (100 - 530 + 1)) + 530;
+		// remove the random number just picked from the b array so it can't be picked again when the loop iterates; this ensures there are no repeating numbers in the column
+		numbers.b.splice(numbers.b.indexOf(numbers.b[randomizeNums]), 1);
+
 		setTimeout(() => {
-			columnB[i].classList.remove('hide')
-		}, 300)
+			columnB[i].innerText = randomNum;
+			columnB[i].classList.replace('hide-number', 'show-number');
+		}, randomNumFade)
+
+		columnB[i].setAttribute('data', `B${randomNum}`);
 	}
 
 	for(let i = 0; i < columnI.length; i++) {
-		let randomizeNums = Math.floor(Math.random() * numbers.i.length),
-		randomNum = numbers.i[randomizeNums],
-		removeRandomNum = numbers.i.indexOf(randomNum);
-		numbers.i.splice(removeRandomNum, 1);
-		bingoSquare = columnI[i];
-		bingoNum = document.createElement('span');
-		bingoNum.classList.add('number', 'hide');
-		bingoNum.innerText = randomNum;
-		bingoSquare.appendChild(bingoNum);
-		bingoNum.parentNode.setAttribute('data', `I${randomNum}`);
+		let randomizeNums = Math.floor(Math.random() * numbers.i.length);
+		let randomNum = numbers.i[randomizeNums];
+		let randomNumFade = Math.floor(Math.random() * (530 - 200 + 1)) + 200;
+		numbers.i.splice(numbers.i.indexOf(numbers.i[randomizeNums]), 1);
+		
 		setTimeout(() => {
-			columnI[i].classList.remove('hide')
-		}, 300)
+			columnI[i].innerText = randomNum;
+			columnI[i].classList.replace('hide-number', 'show-number');
+		}, randomNumFade)
+
+		columnI[i].setAttribute('data', `I${randomNum}`);
 	}
 
 	for(let i = 0; i < columnN.length; i++) {
-		let randomizeNums = Math.floor(Math.random() * numbers.n.length),
-		randomNum = numbers.n[randomizeNums],
-		removeRandomNum = numbers.n.indexOf(randomNum);
-		numbers.n.splice(removeRandomNum, 1);
-		bingoSquare = columnN[i];
-		bingoNum = document.createElement('span');
-		bingoNum.classList.add('number', 'hide');
-		bingoNum.innerText = randomNum;
-		bingoSquare.appendChild(bingoNum);
-		bingoNum.parentNode.setAttribute('data', `N${randomNum}`);
+		let randomizeNums = Math.floor(Math.random() * numbers.n.length);
+		let randomNum = numbers.n[randomizeNums];
+		let randomNumFade = Math.floor(Math.random() * (530 - 200 + 1)) + 200;
+		numbers.n.splice(numbers.n.indexOf(numbers.n[randomizeNums]), 1);
+		
 		setTimeout(() => {
-			columnN[i].classList.remove('hide')
-		}, 300)
+			columnN[i].innerText = randomNum;
+			columnN[i].classList.replace('hide-number', 'show-number');
+		}, randomNumFade)
+
+		columnN[i].setAttribute('data', `N${randomNum}`);
 	}
 
 	for(let i = 0; i < columnG.length; i++) {
-		let randomizeNums = Math.floor(Math.random() * numbers.g.length),
-		randomNum = numbers.g[randomizeNums],
-		removeRandomNum = numbers.g.indexOf(randomNum);
-		numbers.g.splice(removeRandomNum, 1);
-		bingoSquare = columnG[i];
-		bingoNum = document.createElement('span');
-		bingoNum.classList.add('number', 'hide');
-		bingoNum.innerText = randomNum;
-		bingoSquare.appendChild(bingoNum);
-		bingoNum.parentNode.setAttribute('data', `G${randomNum}`);
+		let randomizeNums = Math.floor(Math.random() * numbers.g.length);
+		let randomNum = numbers.g[randomizeNums];
+		let randomNumFade = Math.floor(Math.random() * (530 - 200 + 1)) + 200;
+		numbers.g.splice(numbers.g.indexOf(numbers.g[randomizeNums]), 1);
+		
 		setTimeout(() => {
-			columnG[i].classList.remove('hide')
-		}, 300)
+			columnG[i].innerText = randomNum;
+			columnG[i].classList.replace('hide-number', 'show-number');
+		}, randomNumFade)
+
+		columnG[i].setAttribute('data', `G${randomNum}`);
 	}
 
 	for(let i = 0; i < columnO.length; i++){
-		let randomizeNums = Math.floor(Math.random() * numbers.o.length),
-		randomNum = numbers.o[randomizeNums],
-		removeRandomNum = numbers.o.indexOf(randomNum);
-		numbers.o.splice(removeRandomNum, 1);
-		bingoNum = document.createElement('span');
-		bingoNum.classList.add('number');
-		bingoNum.innerText = randomNum;
-		columnO[i].appendChild(bingoNum);
-		columnO[i].classList.add('hide')
+		let randomizeNums = Math.floor(Math.random() * numbers.o.length);
+		let randomNum = numbers.o[randomizeNums];
+		let randomNumFade = Math.floor(Math.random() * (530 - 200 + 1)) + 200;
+		numbers.o.splice(numbers.o.indexOf(numbers.o[randomizeNums]), 1);
+		
+		setTimeout(() => {
+			columnO[i].innerText = randomNum;
+			columnO[i].classList.replace('hide-number', 'show-number');
+		}, randomNumFade)
+
 		columnO[i].setAttribute('data', `O${randomNum}`);
-		setTimeout(() => {
-			columnO[i].classList.remove('hide')
-		}, 300)
 	}
-
-	squares.forEach(element => {
-		element.classList.add('hide')
-	})
-
-	squareSpans.forEach(element => {
-		setTimeout(() => {
-			element.remove();
-		}, 350)
-	})
 }
 
 document.querySelector('.new-card').addEventListener('click', generateBoardNums);
