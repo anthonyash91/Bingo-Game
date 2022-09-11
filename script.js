8dc09ee990ff072ccd4614db977d5176db6b7b8b
Anthony Ashcraft <anthonyashcraft91@gmail.com>
2022-09-11 00:10:57 -0400

    put all the bingo tile number into an object

--- script.js
+++ script.js
@@ -6,23 +6,21 @@ columnO = document.querySelectorAll('.column-o')
 
 const generateBoardNums = () => {
 	// column b numbers
-	const columnBNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-	// column i numbers
-	columnINums = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
-	// column n numbers
-	columnNNums = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45],
-	// column g numbers
-	columnGNums = [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
-	// column o numbers
-	columnONums = [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75],
+	const numbers = {
+		b: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+		i: [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+		n: [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45],
+		g: [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+		o: [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]
+	},
 	squares = document.querySelectorAll('.square'),
 	squareSpans = document.querySelectorAll('.square span');
 
 	for(let i = 0; i < columnB.length; i++) {
-		let randomizeNums = Math.floor(Math.random() * columnBNums.length),
-		randomNum = columnBNums[randomizeNums],
-		removeRandomNum = columnBNums.indexOf(randomNum);
-		columnBNums.splice(removeRandomNum, 1);
+		let randomizeNums = Math.floor(Math.random() * numbers.b.length),
+		randomNum = numbers.b[randomizeNums],
+		removeRandomNum = numbers.b.indexOf(randomNum);
+		numbers.b.splice(removeRandomNum, 1);
 		bingoSquare = columnB[i];
 		bingoNum = document.createElement('span');
 		bingoNum.classList.add('number', 'hide');
@@ -35,9 +33,10 @@ const generateBoardNums = () => {
 	}
 
 	for(let i = 0; i < columnI.length; i++) {
-		let randomizeNums = Math.floor(Math.random() * columnINums.length),
-		randomNum = columnINums[randomizeNums],removeRandomNum = columnINums.indexOf(randomNum);
-		columnINums.splice(removeRandomNum, 1);
+		let randomizeNums = Math.floor(Math.random() * numbers.i.length),
+		randomNum = numbers.i[randomizeNums],
+		removeRandomNum = numbers.i.indexOf(randomNum);
+		numbers.i.splice(removeRandomNum, 1);
 		bingoSquare = columnI[i];
 		bingoNum = document.createElement('span');
 		bingoNum.classList.add('number', 'hide');
@@ -50,10 +49,10 @@ const generateBoardNums = () => {
 	}
 
 	for(let i = 0; i < columnN.length; i++) {
-		let randomizeNums = Math.floor(Math.random() * columnNNums.length),
-		randomNum = columnNNums[randomizeNums],
-		removeRandomNum = columnNNums.indexOf(randomNum);
-		columnNNums.splice(removeRandomNum, 1);
+		let randomizeNums = Math.floor(Math.random() * numbers.n.length),
+		randomNum = numbers.n[randomizeNums],
+		removeRandomNum = numbers.n.indexOf(randomNum);
+		numbers.n.splice(removeRandomNum, 1);
 		bingoSquare = columnN[i];
 		bingoNum = document.createElement('span');
 		bingoNum.classList.add('number', 'hide');
@@ -66,10 +65,10 @@ const generateBoardNums = () => {
 	}
 
 	for(let i = 0; i < columnG.length; i++) {
-		let randomizeNums = Math.floor(Math.random() * columnGNums.length),
-		randomNum = columnGNums[randomizeNums],
-		removeRandomNum = columnGNums.indexOf(randomNum);
-		columnGNums.splice(removeRandomNum, 1);
+		let randomizeNums = Math.floor(Math.random() * numbers.g.length),
+		randomNum = numbers.g[randomizeNums],
+		removeRandomNum = numbers.g.indexOf(randomNum);
+		numbers.g.splice(removeRandomNum, 1);
 		bingoSquare = columnG[i];
 		bingoNum = document.createElement('span');
 		bingoNum.classList.add('number', 'hide');
@@ -82,10 +81,10 @@ const generateBoardNums = () => {
 	}
 
 	for(let i = 0; i < columnO.length; i++){
-		let randomizeNums = Math.floor(Math.random() * columnONums.length),
-		randomNum = columnONums[randomizeNums],
-		removeRandomNum = columnONums.indexOf(randomNum);
-		columnONums.splice(removeRandomNum, 1);
+		let randomizeNums = Math.floor(Math.random() * numbers.o.length),
+		randomNum = numbers.o[randomizeNums],
+		removeRandomNum = numbers.o.indexOf(randomNum);
+		numbers.o.splice(removeRandomNum, 1);
 		bingoNum = document.createElement('span');
 		bingoNum.classList.add('number');
 		bingoNum.innerText = randomNum;
