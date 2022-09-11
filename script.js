74c27cb6c8ae4f65531c3c3d4f89bf614374d3df
Anthony Ashcraft <anthonyashcraft91@gmail.com>
2022-09-11 12:07:29 -0400

    figuring out a way to reset the winning array

--- script.js
+++ script.js
@@ -2,27 +2,19 @@ const columnB = document.querySelectorAll('.column-b'),
 columnI = document.querySelectorAll('.column-i'),
 columnN = document.querySelectorAll('.column-n'),
 columnG = document.querySelectorAll('.column-g'),
-columnO = document.querySelectorAll('.column-o')
+columnO = document.querySelectorAll('.column-o'),
+bingoCard = document.getElementById('bingo-card')
+
+
 
 const generateBoardNums = () => {
-	// column b numbers
-	const numbers = {b: [], i: [], n: [], g: [], o: []},
+	const numbers = {b: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], i: [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30], n: [31,32,33,34,35,36,37,38,39,40,41,42,43,45,45], g: [46,47,48,49,50,51,52,53,54,55,56,57,58,59,60], o: [61,62,63,64,65,66,67,68,69,70,71,72,73,74,75]},
 	squares = document.querySelectorAll('.square');
 
 	squares.forEach(element => {
 		element.classList.replace('show-number', 'hide-number');
+		element.classList.remove('hit');
 	})
-
-	// create the array with the numbers that will be randomly picked and assigned to spots in the B column
-	for(let i = 1; i <= 15; i++) numbers.b.push(i);
-	// create the array with the numbers that will be randomly picked and assigned to spots in the I column
-	for(let i = 16; i <= 30; i++) numbers.i.push(i);
-	// create the array with the numbers that will be randomly picked and assigned to spots in the N column
-	for(let i = 31; i <= 45; i++) numbers.n.push(i);
-	// create the array with the numbers that will be randomly picked and assigned to spots in the G column
-	for(let i = 46; i <= 60; i++) numbers.g.push(i);
-	// create the array with the numbers that will be randomly picked and assigned to spots in the O column
-	for(let i = 61; i <= 75; i++) numbers.o.push(i);
 
 	for(let i = 0; i < columnB.length; i++) {
 		let randomizeNums = Math.floor(Math.random() * numbers.b.length);
@@ -98,3 +90,40 @@ const generateBoardNums = () => {
 }
 
 document.querySelector('.new-card').addEventListener('click', generateBoardNums);
+
+let winningNumbers = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'B11', 'B12', 'B13', 'B14', 'B15', 'I16', 'I17', 'I18', 'I19', 'I20', 'I21', 'I22', 'I23', 'I24', 'I25', 'I26', 'I27', 'I28', 'I29', 'I30', 'N31', 'N32', 'N33', 'N34', 'N35', 'N36', 'N37', 'N38', 'N39', 'N40', 'N41', 'N42', 'N43', 'N44', 'N45', 'G46', 'G47', 'G48', 'G49', 'G50', 'G51', 'G52', 'G53', 'G54', 'G55', 'G56', 'G57', 'G58', 'G59', 'G60', 'O61', 'O62', 'O63', 'O64', 'O65', 'O66', 'O67', 'O68', 'O69', 'O70', 'O71', 'O72', 'O73', 'O74', 'O75'];
+
+
+let chosenNums = [];
+
+const reset = () => {
+	
+	for (let i = 0; i < chosenNums.length; i++) {
+			winningNumbers.push(chosenNums[i])
+			chosenNums.splice(chosenNums.indexOf(chosenNums[i]), 1);
+	}
+
+}
+
+
+
+const pickWinningNum = () => {
+
+	let randomizeWinningNumbers = Math.floor(Math.random() * winningNumbers.length);
+	let winningNumber = winningNumbers[randomizeWinningNumbers];
+	winningNumbers.splice(winningNumbers.indexOf(winningNumbers[randomizeWinningNumbers]), 1);
+
+	bingoCard.addEventListener('click', (evt) => {
+		if(evt.target.getAttribute('data') === winningNumber) {
+			evt.target.classList.add('hit');
+		}
+	})
+
+	document.querySelector('#winning-number').innerText += winningNumber;
+	chosenNums.push(winningNumber);
+	console.log(chosenNums)
+	console.log(winningNumbers)
+}
+
+document.querySelector('.choose-winning-number').addEventListener('click', pickWinningNum);
+document.querySelector('.reset').addEventListener('click', reset);
