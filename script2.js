749e90dfa1c8e0b1a1206fe2576a5e1787cf3dbf
Anthony Ashcraft <anthonyashcraft91@gmail.com>
2022-09-15 00:38:24 -0400

    edited js

--- script2.js
+++ script2.js
@@ -1,24 +1,23 @@
 const tiles = document.querySelectorAll('.tile'),
-			playerHeaderTiles = document.querySelectorAll('#player .header-tile'),
-			computerTiles = document.querySelectorAll('#computer .tile'),
-			playerTiles = document.querySelectorAll('#player .tile'),
-			player = document.getElementById('player'),
-			computer = document.getElementById('computer'),
-			generateCardsButton = document.querySelector('.generate-cards'),
-			bingoCards = document.querySelectorAll('.bingo-card'),
-			playerNames = document.querySelectorAll('.player-name'),
-			playerNameSpans = document.querySelectorAll('.player-name span'),
-			winningNumbersDiv = document.getElementById('winning-number'),
-			chooseNumbersDiv = document.getElementById('choose-numbers'),
-			chooseWinningNumberButton = document.querySelector('.choose-winning-number');
+playerHeaderTiles = document.querySelectorAll('#player .header-tile'),
+computerTiles = document.querySelectorAll('#computer .tile'),
+playerTiles = document.querySelectorAll('#player .tile'),
+player = document.getElementById('player'),
+computer = document.getElementById('computer'),
+generateCardsButton = document.querySelector('.generate-cards'),
+bingoCards = document.querySelectorAll('.bingo-card'),
+playerNames = document.querySelectorAll('.player-name'),
+playerNameSpans = document.querySelectorAll('.player-name span'),
+winningNumbersDiv = document.getElementById('winning-number'),
+chooseNumbersDiv = document.getElementById('choose-numbers'),
+chooseWinningNumberButton = document.querySelector('.choose-winning-number');
 
 let autoplay = false;
-
-// this will count up by 1 each time a tile is iterated over; this is is used to determine which array inside the bingoNumbers object to use when choosing a random number and placing it into a tile
 
 let winningNumbers = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'B11', 'B12', 'B13', 'B14', 'B15', 'I16', 'I17', 'I18', 'I19', 'I20', 'I21', 'I22', 'I23', 'I24', 'I25', 'I26', 'I27', 'I28', 'I29', 'I30', 'N31', 'N32', 'N33', 'N34', 'N35', 'N36', 'N37', 'N38', 'N39', 'N40', 'N41', 'N42', 'N43', 'N44', 'N45', 'G46', 'G47', 'G48', 'G49', 'G50', 'G51', 'G52', 'G53', 'G54', 'G55', 'G56', 'G57', 'G58', 'G59', 'G60', 'O61', 'O62', 'O63', 'O64', 'O65', 'O66', 'O67', 'O68', 'O69', 'O70', 'O71', 'O72', 'O73', 'O74', 'O75'];
 
 let chosenNums = [];
+// this will count up by 1 each time a tile is iterated over; this is is used to determine which array inside the bingoNumbers object to use when choosing a random number and placing it into a tile
 let tileCounter = 0;
 
 setTimeout(() => {
