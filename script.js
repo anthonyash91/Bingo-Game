8a32a4751ebb1b60b185cadea68f6c3e57b8b961
Anthony Ashcraft <anthonyashcraft91@gmail.com>
2022-09-20 19:02:50 -0400

    finished

--- script.js
+++ script.js
@@ -89,7 +89,11 @@ noThanksButton.addEventListener('click', (evt) => {
 
 const generateCards = () => {
 	let bingoNumbersArrays = {
-		b: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], i: [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30], n: [31,32,33,34,35,36,37,38,39,40,41,42,43,44,45], g: [46,47,48,49,50,51,52,53,54,55,56,57,58,59,60], o: [61,62,63,64,65,66,67,68,69,70,71,72,73,74,75]
+		b: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], 
+		i: [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30], 
+		n: [31,32,33,34,35,36,37,38,39,40,41,42,43,44,45], 
+		g: [46,47,48,49,50,51,52,53,54,55,56,57,58,59,60], 
+		o: [61,62,63,64,65,66,67,68,69,70,71,72,73,74,75]
 	}
 
 	const bingoKeys = Object.keys(bingoNumbersArrays);
@@ -496,15 +500,8 @@ const drawNumber = () => {
 	}, 400)
 }
 
-
-
-
 generateCardsButton.addEventListener('click', generateCards);
 drawNumberButton.addEventListener('click', drawNumber);
-
-
-
-
 
 const confettiCannon = () => {
 		for(let i = 0; i < 500; i++) {
@@ -551,14 +548,6 @@ const removeConfetti = () => {
 		setTimeout(() => {element.remove();}, 500);
 	})
 }
-
-
-
-
-
-
-
-
 
 const resetGame = () => {
 	for(let i of chosenWinningNumbersArray) {
